9ed20e785d6a4c2cb1019013d5c358815a0084fb
Bryan1739 <bryan173903@gmail.com>
2023-03-09 16:02:24 -0600

    first commit

---
+++ src/Models/Vendedor.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Uniqua
+ */
+public class Vendedor {
+    private String codigo;
+    private String nombre;
+    private int caja;
+    private int ventas;
+    private String genero;
+    private String password;
+    
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the caja
+     */
+    public int getCaja() {
+        return caja;
+    }
+
+    /**
+     * @param caja the caja to set
+     */
+    public void setCaja(int caja) {
+        this.caja = caja;
+    }
+
+    /**
+     * @return the ventas
+     */
+    public int getVentas() {
+        return ventas;
+    }
+
+    /**
+     * @param ventas the ventas to set
+     */
+    public void setVentas(int ventas) {
+        this.ventas = ventas;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
